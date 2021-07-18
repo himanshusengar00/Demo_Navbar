3544380ffff6d5afe1cd2040582ff2cb71a65e57
himanshusengar00 <49912009+himanshusengar00@users.noreply.github.com>
2021-07-18 12:35:49 +0530

    Add files via upload

---
+++ navbar/src/components/Navbar/MenuItems.js
@@ -0,0 +1,30 @@
+export const MenuItems = [
+ 
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'Nav-links'
+
+    },
+    {
+        title: 'Menu',
+        url: '#',
+        cName: 'Nav-links'
+
+    },
+    {
+        title: 'Offers',
+        url: '#',
+        cName: 'Nav-links'
+
+    },
+    {
+        title: 'Our Services',
+        url: '#',
+        cName: 'Nav-links'
+
+    }
+
+
+
+]
